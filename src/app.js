f75a705eaa9a99d4e05d0d90da68d673b5b8c751
champ-sk <shubhamkumar3183@gmail.com>
2026-02-25 23:14:49 +0530

    adding schema and signup api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const connectDB = require('./config/database');
+const User = require('./models/user');  
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
-app.listen(3000, () => {
+
+app.post('/user', async (req,res)=>{
+    const user = new User( {
+        firstName: "Shubham" , 
+        lastName: "Kumar" ,
+        age: 22 ,
+        emailID: "shubhamkumar3183@gmail.com", 
+        password: "shubham123"
+
+    });
+    try{
+        const savedUser = await user.save();
+        res.status(201).json({message: "User created successfully", user: savedUser});
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+})
+connectDB().then(()=>{
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+}).catch((error)=>{
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1);
+});
+
