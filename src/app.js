c61c2ac6d3a18d1ae526340c28ab8652a6298584
champ-sk <shubhamkumar3183@gmail.com>
2026-02-26 15:45:03 +0530

    adding schema and db level validation.

--- src/app.js
+++ src/app.js
@@ -2,27 +2,89 @@ const express = require('express');
 const connectDB = require('./config/database');
 const User = require('./models/user');  
 const app = express();
-
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.post('/user', async (req,res)=>{
-    const user = new User( {
-        firstName: "Shubham" , 
-        lastName: "Kumar" ,
-        age: 22 ,
-        emailID: "shubhamkumar3183@gmail.com", 
-        password: "shubham123"
-
-    });
+app.post('/signup', async (req,res)=>{
+    const user = new User( req.body);
     try{
         const savedUser = await user.save();
         res.status(201).json({message: "User created successfully", user: savedUser});
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
-})
+});
+
+//get user by emailid
+app.get('/user', async (req, res) => {
+
+   const email = req.query.emailId;
+   try{
+    const user = await User.findOne({ emailId: email });
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }   else {      
+        res.json(user);
+     }  
+   }
+    catch(error){
+        res.status(500).json({ error: error.message });
+    }
+
+ 
+});
+
+//get all users
+app.get('/feed' , async (req, res)=>{
+    try{
+        const users = await User.find();
+        res.json(users);
+    }   catch(error){   
+        res.status(500).json({ error: error.message });
+    }
+});
+
+//delete user by emailid
+app.delete('/user', async(req, res)=>{
+    const email =req.query.emailId;
+    try{
+        const deletedUser = await User.findOneAndDelete({ emailId: email });
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        } else {
+            res.json({ message: 'User deleted successfully', user: deletedUser });
+        }           
+    }
+    catch(error){
+        res.status(500).json({ error: error.message });
+    }    
+});
+// update user by userid
+app.patch('/user', async(req, res)=>{
+    const userId =req.query.userId;
+    
+    try{
+        const AllowedUpdates = [ 'password', 'photourl', 'skills', 'gender'];
+        const updates  = Object.keys(req.body).every((k)=> AllowedUpdates.includes(k));
+        if(!updates){
+            return res.status(400).json({ message: 'Invalid updates!' });
+        }
+        
+        const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });     
+        } else {
+            res.json({ message: 'User updated successfully', user: updatedUser });
+        }
+    }
+    catch(error){
+        res.status(500).json({ error: error.message });
+    }
+});
+
+
 connectDB().then(()=>{
     console.log('Connected to MongoDB');
     app.listen(3000, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,  
+        required: true,
     },
     lastName:{
         type: String,
@@ -11,10 +13,44 @@ const userSchema = new mongoose.Schema({
     },
     emailID:{
         type: String,
+        required: true,
+        unique: true,
+        trim: true , 
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email address');
+            }
+        },
+    
     },
     password:{
         type: String,   
-    }   
-});
+        required: true,
+        validate(value){
+            if (validator.isStrongPassword(value, { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })) {
+                return true;
+              } else {
+                throw new Error('Password must be at least 8 characters long and include uppercase letters, lowercase letters, numbers, and symbols.');
+              } 
+        }
+    }    ,
+    photourl:{
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    skills:{
+        type: [String],
+    },
+    gender:{
+        type: String,
+        validate(value){
+            if(!['Male', 'Female',   'Other'].includes(value)){
+                throw new Error('Invalid gender value');
+            }
+        },
+    },
+
+}, { timestamps: true });
 const User = mongoose.model('User', userSchema);
 module.exports = User;
