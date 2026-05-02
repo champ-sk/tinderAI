8d6718872e274acc1b121180b9af0ff50737ed79
Shubham kumar Agrawal <68751450+champ-sk@users.noreply.github.com>
2026-05-02 11:03:44 +0530

    add netlify redirects, update the BASE_URL

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-//export const BASE_URL  =  "https://tinderai-wb2h.onrender.com";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL  =  "https://tinderai-wb2h.onrender.com";
+//export const BASE_URL = "http://localhost:3000";
